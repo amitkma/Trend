39e27e3e0c1b2da91318bf6dcbe878c678c91e69
Amit Kumar <mr.doc10jl96@gmail.com>
2016-01-09 17:40:01 +0530

    Create: New Activity DetailsActivity Added to the project

---
+++ app/src/main/java/com/arish/trend/DetailsActivity.java
@@ -0,0 +1,37 @@
+package com.arish.trend;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.parse.ParseUser;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        textView = (TextView)findViewById(R.id.textView);
+        ParseUser user = ParseUser.getCurrentUser();
+        textView.setText(user.getUsername());
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
