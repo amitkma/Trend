13123bc1ec92c50a17e85fec044751bc79f6a98f
Amit Kumar <mr.doc10jl96@gmail.com>
2016-01-09 17:55:53 +0530

    Add parse login and signup

--- app/src/main/java/com/arish/trend/MainActivity.java
+++ app/src/main/java/com/arish/trend/MainActivity.java
@@ -1,5 +1,6 @@
 package com.arish.trend;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,15 +9,31 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button loginButton;
+    private Button signupButton;
+    private EditText usernameField;
+    private EditText userPasswordField;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginButton = (Button)findViewById(R.id.loginButtonId);
+        signupButton = (Button)findViewById(R.id.signupButtonId);
+        usernameField = (EditText)findViewById(R.id.username);
+        userPasswordField = (EditText)findViewById(R.id.userPassword);
 
         testParse();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -30,8 +47,49 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginParse();
+            }
+        });
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signupParse();
+            }
+        });
     }
 
+    private void loginParse() {
+        ParseUser.logInInBackground(usernameField.getText().toString().trim(), userPasswordField.getText().toString().trim(), new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(e==null){
+                    Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+
+    private void signupParse(){
+        ParseUser parseUser = new ParseUser();
+        parseUser.setUsername(usernameField.getText().toString().trim());
+        parseUser.setPassword(userPasswordField.getText().toString().trim());
+
+        parseUser.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
     private void testParse() {
         ParseObject testObject = new ParseObject("TestObject");
         testObject.put("foo", "bar");
