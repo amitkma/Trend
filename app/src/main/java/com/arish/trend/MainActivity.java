ca9ce6528f9d37c880a66a91f00b5bfb1d795e55
Ayush P Gupta <ayushpguptaapg@gmail.com>
2016-01-10 19:05:58 +0530

    updated login parseexception methods in login and signup

--- app/src/main/java/com/arish/trend/MainActivity.java
+++ app/src/main/java/com/arish/trend/MainActivity.java
@@ -57,7 +57,7 @@ public class MainActivity extends BaseActivity {
             public void onClick(View v) {
                 if (check_connection() == false)
                     Snackbar.make(findViewById(R.id.coordinatorlay), R.string.network_state_false, Snackbar.LENGTH_SHORT).show();
-                else if (usernameField.getText().toString().trim().length() != 0 && userPasswordField.getText().toString().trim().length() > 5)
+                else if (usernameField.getText().toString().trim().length() != 0 && userPasswordField.getText().toString().trim().length() > R.integer.min_char_for_password)
 
                     signupParse();
                 else if (userPasswordField.getText().toString().trim().length() == 0)
