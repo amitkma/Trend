bb3b3f24aeedbe566722a82d450012ab9dd2d6e5
Ayush P Gupta <ayushpguptaapg@gmail.com>
2016-01-13 16:34:49 +0530

    added nav drawer

--- app/src/main/java/com/arish/trend/BaseActivity.java
+++ app/src/main/java/com/arish/trend/BaseActivity.java
@@ -1,12 +1,107 @@
 package com.arish.trend;
 
 import android.net.ConnectivityManager;
+import android.os.Handler;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 /**
  * Created by Ayush on 10-Jan-16.
  */
 public class BaseActivity extends AppCompatActivity {
+
+
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+
+
+    public void setup_toolbar() {
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+
+    //Navigatio Drawer
+    public void setup_nav_drawer() {
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.openDrawer, R.string.closeDrawer) {
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                //super.onDrawerClosed(drawerView);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // super.onDrawerOpened(drawerView);
+            }
+        };
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        setup_nav_item_listener();
+    }
+
+    public void setup_nav_item_listener() {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                //Checking if the item is in checked state or not, if not make it in checked state
+              /*  if (menuItem.isChecked())
+                    menuItem.setChecked(false);
+                else
+                    menuItem.setChecked(true);*/
+
+
+                switch (menuItem.getItemId()) {
+
+                    case R.id.About:
+                        drawerLayout.closeDrawers();
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+
+                            }
+                        }, 150);
+                        return true;
+
+                    case R.id.logout:
+                        drawerLayout.closeDrawers();
+
+
+                        return true;
+                    case R.id.Feedback:
+                        drawerLayout.closeDrawers();
+                        new Handler().postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+
+                            }
+                        }, 500);
+
+                        return true;
+
+
+                    default:
+
+                        return true;
+
+                }
+            }
+        });
+    }
 
 
 
--- app/src/main/java/com/arish/trend/CurrentTrendsActivity.java
+++ app/src/main/java/com/arish/trend/CurrentTrendsActivity.java
@@ -3,20 +3,19 @@ package com.arish.trend;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class CurrentTrendsActivity extends AppCompatActivity {
+public class CurrentTrendsActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_trends);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
+        setup_toolbar();
+        setup_nav_drawer();
+        setup_nav_item_listener();
+        View view=navigationView.inflateHeaderView(R.layout.nav_header);
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
